db5343180b71fb5b3e67ed7d708a281583bc79dd
ramsay <ramsaylanier@gmail.com>
2014-02-13 01:46:23 -0500

    started styling admin editor - refined page listing
    Signed-off-by: ramsay <ramsaylanier@gmail.com>

--- client/views/pages/page_list.js
+++ client/views/pages/page_list.js
@@ -1,5 +1,20 @@
 Template.pagesList.helpers({
 	pages: function(){
 		return Pages.find();
+	},
+	prettyDate: function(submitted){
+		return moment(new Date(submitted)).format('MMMM Do, YYYY');
+	}
+});
+
+Template.pagesList.events({
+	'click .delete-page' : function(e) {
+		e.preventDefault();
+
+		if (confirm("Delete This Page?")){
+			var currentPageId = this._id;
+			Pages.remove(currentPageId);
+			Router.go('/admin/pages');
+		}
 	}
 });
