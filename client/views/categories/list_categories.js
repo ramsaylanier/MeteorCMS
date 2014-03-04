b036c5d86ce935ec87da826a9cca4de708a51fc0
ramsay <ramsaylanier@gmail.com>
2014-03-04 02:32:40 -0500

    finished post category functionality updates to remove categories from posts when category is deleted
    Signed-off-by: ramsay <ramsaylanier@gmail.com>

--- client/views/categories/list_categories.js
+++ client/views/categories/list_categories.js
@@ -2,7 +2,22 @@ Template.listCategories.helpers({
 	categories: function(){
 		return Categories.find();
 	},
-	postCount: function(){
-		
+	postCount: function(name){
+		var posts = Posts.find({categories: name});
+		return posts.count();
 	}
 });
+
+Template.listCategories.events({
+	'click .delete-category': function(e){
+		e.preventDefault();
+		var category = this.name;
+		Categories.remove(this._id);
+
+		var posts = Posts.find({categories: this.name}).fetch();
+		for (var i=0;i<posts.length;i++){
+			var cats = _.without(posts[i].categories, this.name);
+			Posts.update(posts[i]._id, {$set: {categories: cats}});
+		}		
+	}
+})
